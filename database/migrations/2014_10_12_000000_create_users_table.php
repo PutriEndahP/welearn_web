e1e764aa7bd44bc4dcdd6ab2aa1d853ce2edb762
Yasinta Yusniawati <yasintayusniawati104@gmail.com>
2021-04-07 17:56:27 +0700

    create migration level dan memperbaiki users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,12 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username', 255);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->date('tanggal_lahir')->nullable();
+            $table->char('jenis_kelamin', 1);
             $table->rememberToken();
             $table->timestamps();
         });
