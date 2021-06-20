d1a2d63b155b0c8b5d480eef7253bc7a02f4fddd
Yasinta Yusniawati <yasintayusniawati104@gmail.com>
2021-06-20 13:44:52 +0700

    fix maxHuruf dan maxAngka

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,26 +12,22 @@ class HomeController extends Controller
 {
     public function show()
     {
-        // $max = DB::tabel('score', 1)->max('id_jenis');
-        // $data['max'] = DB::where('id_jenis', 1) 
-        // ->orderBy('score', 'dsc')
-        // ->take(1) 
-        // ->get();
-
         $data['count'] = DB::table('users')->count();
-        // $max['maxHuruf'] = DB::table('score')->join('soal', 'score.id_soal', '=', 'soal.id_soal')->where('soal.id_jenis', 1)
-        //  ->where('score.id_user', '=' ('score.id_user')->sum('score.score')->max('score.score'));
-         
-        //  1)->sum('score.score');
-        // $maxHuruf['maxHuruf'] = DB::table('score')->max('score')->join('soal', 'score.id_soal', '=', 'soal.id_soal')
-        // ->where('soal.id_jenis', 1)->sum('score.score')
-        // ->groupBy('score.id_user', 1);
 
-        $maxHuruf['maxHuruf']= DB::table('score')->join('soal', 'score.id_soal', '=', 'soal.id_soal')
+
+        $maxHuruf = DB::table('score')->join('soal', 'score.id_soal', '=', 'soal.id_soal')
         ->select(DB::raw('sum(score.score) as total_score'))
         ->where('soal.id_jenis', 1)
         ->groupBy('score.id_user')
-        ->orderBy('score.id_user', 'DESC')->get();
+        ->orderBy('score.score', 'asc')->get();
+
+        foreach($maxHuruf as $mx)
+        {$dat = $mx;}
+
+        $temp = (array) $dat;
+        foreach($temp as $tmp)
+        {$huruf = $tmp;}
+        $dataMax['maxHuruf'] = $huruf;
 
 //         $maxHuruf['maxHuruf']=DB::table('score')
 // ->join(
@@ -46,15 +42,22 @@ class HomeController extends Controller
         // ->first();
         // ->get(max(['score.id_user']));
 
-        // $maxAngka['maxAngka']= DB::table('score')->join('soal', 'score.id_soal', '=', 'soal.id_soal')
-        // ->select(DB::raw('sum(score.score) as total_score'))
-        // ->where('soal.id_jenis', 2)
-        // ->groupBy('score.id_user')
-        // ->orderBy('score.id_user', 'DESC')->get();
+        $maxAngka= DB::table('score')->join('soal', 'score.id_soal', '=', 'soal.id_soal')
+        ->select(DB::raw('sum(score.score) as total_score'))
+        ->where('soal.id_jenis', 2)
+        ->groupBy('score.id_user')
+        ->orderBy('score.score', 'asc')->get();
         // ->first();
         // ->get(max(['score.id_user']));
+        foreach($maxAngka as $mx)
+        {$dang = $mx;}
 
-        return view('home.home', $data, $maxHuruf);
+        $tank = (array) $dang;
+        foreach($tank as $tmp)
+        {$angka = $tmp;}
+        $dataMax['maxAngka'] = $angka;
+        
+        return view('home.home', $data, $dataMax);
         // print_r($maxHuruf);die();
         // print_r($maxHuruf['maxHuruf'][0]->total_score);die;
         // echo '<pre>';print_r($maxHuruf);echo '</pre>';die();
